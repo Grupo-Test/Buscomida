470ef63727234b548c8ed4a7dd7f5647d03103e2
Cesar <Cesar@Cesar-PC>
2011-07-31 10:07:52 -0500

    Final Test

--- src/edu/cibertec/buscomida/test/selenium/TestRegistrarRestauranteOk.java
+++ src/edu/cibertec/buscomida/test/selenium/TestRegistrarRestauranteOk.java
@@ -17,7 +17,7 @@ public class TestRegistrarRestauranteOk extends SeleneseTestBase {
 	
 	@Before
 	public void setUp() throws Exception {
-		setUp("http://localhost:8080/buscomida/", "*firefox", Integer.valueOf(SELENIUM_SERVER_PORT));
+		setUp("http://localhost:8080/", "*firefox", Integer.valueOf(SELENIUM_SERVER_PORT));
 		//WebDriver driver = new ChromeDriver();
 		//String baseUrl = "http://localhost:8080/";
 		//Selenium selenium = new WebDriverBackedSelenium(driver, baseUrl);
